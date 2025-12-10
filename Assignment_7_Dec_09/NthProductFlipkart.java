2f57bd7f254a1fd241cfce831d0822cf4252ab04
Sanjith-Meenakshi-Sundaram <sanjithdhamu@gmail.com>
2025-12-10 13:24:56 +0530

    Assignment 7

--- Assignment_7_Dec_09/NthProductFlipkart.java
+++ Assignment_7_Dec_09/NthProductFlipkart.java
@@ -1,29 +1,36 @@
 package basicprogram;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 import java.util.List;
 
 public class NthProductFlipkart{
     public static void main(String[] args) throws Exception {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://www.flipkart.com");
+        
+        driver.findElement(By.xpath("//input[contains(@placeholder,'Search for Products, Brands and More')]")).click();
+        driver.findElement(By.xpath("//input[contains(@placeholder,'Search for Products, Brands and More')]")).sendKeys("Laptop");
+        driver.findElement(By.xpath("//input[contains(@placeholder,'Search for Products, Brands and More')]")).sendKeys(Keys.ENTER);
+        driver.findElement(By.className("Vy9RSP")).click();
         Thread.sleep(2000);
-        String[] products={"Laptop","TV","Smart Phone"};
-        int[] nth={7,13,2};
-        for(int i=0;i<products.length;i++){
-            WebElement searchBox=driver.findElement(By.name("q"));
-            searchBox.clear();
-            searchBox.sendKeys(products[i]);
-            searchBox.submit();
-            Thread.sleep(3000);
-            List<WebElement> resultNames=driver.findElements(By.cssSelector("div._4rR01T, a.s1Q9rs"));
-            
-            Thread.sleep(1500);
-        }
-        driver.quit();
+        driver.findElement(By.className("Vy9RSP")).clear();
+        Thread.sleep(2000);
+        driver.findElement(By.className("Vy9RSP")).sendKeys("TV");
+        driver.findElement(By.className("Vy9RSP")).sendKeys(Keys.ENTER);
+        driver.findElement(By.className("Vy9RSP")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.className("Vy9RSP")).clear();
+        Thread.sleep(2000);
+        driver.findElement(By.className("Vy9RSP")).sendKeys("Smart Phone");
+        driver.findElement(By.className("Vy9RSP")).sendKeys(Keys.ENTER);
+//        driver.quit();
     }
 }
